3b573e596b30a569161d08737ad9864effdc4985
Lorna Timbah <me@webgrrrl.net>
2018-03-30 20:57:59 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,7 +5,7 @@ boomerang.factory('Config', function () {
         'id'            : '117368911868983524073',
         'google_api'    : 'AIzaSyA9ALjr2iWvhf3Rsz9-bH0cEcDcrdkpuAg',
         'pwa_id'        : 'c0v95n2me2i4gq9ioaij0lur8vc', //picasa web album id, must belong to google+ id above
-        'domain'        : 'http://www.gdgkk.info',
+        'domain'        : 'http://gdgkotakinabalu.com',
         'cover' : {
             title: 'Google Developer Group Kota Kinabalu',
             subtitle: 'GDG in the city where the summit, sea, & sunset meet',
